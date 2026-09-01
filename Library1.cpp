d25daf00705fc084dcc7770b9ee0f519caf456f8
Adhvitya Pathare <adhvityapathare548@gmail.com>
2026-09-01 14:46:16 +0530

    Implement book ID search functionality
    Added a C++ program to search for a book ID in an array and display if it is found or not.

--- Library1.cpp
+++ Library1.cpp
@@ -45,3 +45,32 @@ cout<<"\nBook Title: "<<title3;
 
 return 0;
 }
+
+#2 Searching a book ID
+//Write a C++ program of books with ID in an Array and search for a given book ID. Display "BookFound"if ID is present
+//otherwise not present 
+#include<iostream>
+using namespace std;
+
+int main(){
+int book[5];
+int searchID;
+
+cout<<"Enter 5 Book IDs: \n";
+
+for(int i=0; i<5;i++){
+cin>> book i;}
+
+
+cout<<"\nEnter BookID to search: ";
+cin>>searchID;
+
+for(int i = 0; i<5;i++){
+if(book[i]==searchID);
+cout<<"Book Found";
+return 0;}
+
+
+cout<<"Book not found";
+return 0;
+}
